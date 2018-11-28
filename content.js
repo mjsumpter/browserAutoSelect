dc359a2c359986196db88a37265447f99a7d1d51
mjsumpter <mjsumpter@gmail.com>
2018-11-28 15:34:48 -0500

    Fixed text selection function to better identify highlighted text when right-clicking

--- content.js
+++ content.js
@@ -1,20 +1,33 @@
+function grabText() {
+	var text = "";
+	if (typeof window.getSelection != "undefined") {
+		text = window.getSelection().toString();
+	}
+	else if (document.selection.type == "Text" && typeof document.selection != "undefined")
+	{
+		text = document.selection.createRange().text;
+	}
+
+	return text;
+}
+
+
+// auto copy selected text
 document.addEventListener("selectionchange", () => {
 
 	document.addEventListener("mouseup", () => {
 		document.execCommand("copy");
-	})
+	});
 });
 
+// send selected text as search query to Google
 document.addEventListener("contextmenu", (e) => {
-	if (e.target.tagName !== "IMG" && e.target.tagName !== 'A')
+	var textSelected = grabText();
+	console.log(textSelected);
+	if (textSelected)
 	{
 		e.preventDefault();
-		const selectedText = window.getSelection().toString();
-		window.open('http://google.com/search?q=' + selectedText);
+		window.open('http://google.com/search?q=' + textSelected);
 		return false;
 	}
 }, false);
-
-
-
-//clear clipboard after 10 seconds
